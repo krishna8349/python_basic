3fc8dc75c47d5609a8d134b2fe54676df1153ad8
krishna8349 <ksisodiya196@gmail.com>
2021-12-01 10:57:27 +0530

    For checking factorial

---
+++ Basic/factorial.py
@@ -0,0 +1,13 @@
+def fectorial(n):
+    if n<0:
+        return 0
+    elif n==0 or n==1:
+        return 1
+    else:
+        fact = 1
+        while (n>1):
+            fact*=n
+            n -= 1
+        print("factorial is ",fact)
+
+fectorial(5)
