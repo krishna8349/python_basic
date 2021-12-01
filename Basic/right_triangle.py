ebd82094e916a8e1e1731b192cc96d3f6e40acb8
krishna8349 <ksisodiya196@gmail.com>
2021-12-01 16:40:53 +0530

    Program of Right triangle

---
+++ Basic/right_triangle.py
@@ -0,0 +1,20 @@
+def right_triangle(n):
+    #smallest way
+    for i in range(0,n):
+        print(" "*(n-i-1), "*"*(i+1))
+
+    #Normal way
+    # space = 2*n-2
+
+    # for i in range(0,n):
+    #     for j in range(0,space):
+    #         print(end=" ")
+
+    #     space = space-2
+    
+    #     for j in range(0,i+1):
+    #         print("* ",end="")
+
+    #     print("\r")
+
+right_triangle(5)
