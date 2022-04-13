ec1c99943bb4cad460ecb7d68607950a0966993a
Krishna <ksisodiya196@gmail.com>
2022-04-13 17:07:44 +0530

    inheritance a class

---
+++ opps_concept/inheritance.py
@@ -0,0 +1,22 @@
+class Car:
+
+    def __init__(self, name, color, price, city):
+        self.name = name
+        self.color = color
+        self.price = price
+        self.city = city
+
+    def add(self,name,color, price, city):
+        self.name
+        self.color
+        self.price
+        self.city
+
+car = Car('Audi','Black', 5000000, 'Indore')
+# print(car.color)
+
+class Second(Car):
+    pass
+
+second =  Second('Mustange', 'Yello', 8000000, 'Delhi')
+print(second.city)
