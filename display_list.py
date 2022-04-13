6dc16e90859a2dbd5fe747ff0b9255d7f5ad136c
Krishna <ksisodiya196@gmail.com>
2022-04-13 16:40:55 +0530

    display the list

---
+++ display_list.py
@@ -0,0 +1,6 @@
+color_list = ["Red","Green","White","Black"]
+
+print("First color is = "+color_list[0] +" and Last color is = "+color_list[-1])
+
+exam_date = (11,12,2021)
+print("The examination will start from : %i/%i/%i"%exam_date)
