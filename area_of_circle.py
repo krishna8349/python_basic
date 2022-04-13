85853036aaace3ce65e0186a4a3c2d84db0688d6
Krishna <ksisodiya196@gmail.com>
2022-04-13 16:38:51 +0530

    area of circle

---
+++ area_of_circle.py
@@ -0,0 +1,22 @@
+from math import pi
+from turtle import st
+
+# Area = pir2
+r = float(input("Enter a radious :"))
+print("Area = "+ str(pi*r**2))
+print("-------------------------------")
+
+# Area of rectangle = length * weight
+
+l = float(input("Enter a length of rectangle = "))
+w = float(input("Enter a weight of the ranctangle = "))
+
+print("Area of rectange = "+str(l*w))
+print("-------------------------------")
+# Area of perimeter = a * b * c
+
+a = float(input("Enter a value of a = "))
+b = float(input("Enter a value of b = "))
+c = float(input("Enter a value of c = "))
+
+print("Area of perimeter is = "+str(a*b*c))
